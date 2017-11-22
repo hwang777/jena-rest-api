dcb03e216dfd9d15e7a022161c9b61a15545ea45
hwang777 <hw2@hotmail.com>
2017-11-21 21:52:43 -0600

    Test

---
+++ src/test/java/com/hw/db/TestAPI.java
@@ -0,0 +1,24 @@
+package com.hw.db;
+
+import org.neo4j.driver.v1.*;
+import org.neo4j.helpers.collection.MapUtil;
+
+import static org.neo4j.driver.v1.Values.parameters;
+
+import java.util.Map;
+
+import org.junit.Test;
+public class TestAPI
+{
+	@Test
+	public void test1()
+	{
+		
+	}
+	@Test
+	public void test2()
+	{
+		
+	}
+
+}
